62572b94861877e8a9f6362d1af7070bbb905f1e
Gui2761 <guilhermesilva2761@gmail.com>
2026-04-24 11:03:34 -0300

    Removendo arquivos temporários e testes a pedido do usuário

--- list_sheets.ps1
+++
@@ -1,7 +0,0 @@
-$excel = New-Object -ComObject Excel.Application
-$excel.Visible = $false
-$excel.DisplayAlerts = $false
-$workbook = $excel.Workbooks.Open('C:\Users\Guilh\folha_pagamento_itps\01 JANEIRO - 2026.xlsx')
-foreach ($s in $workbook.Sheets) { Write-Host $s.Name }
-$workbook.Close($false)
-$excel.Quit()
